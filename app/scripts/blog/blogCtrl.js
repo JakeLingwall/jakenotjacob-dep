1360194c12c814e00f3edcd0a03c7e11d0dee156
Jake Lingwall <jake.lingwall@domo.com>
2014-04-03 22:08:06 -0600

    I wrote a screenplay instead of this site.

--- app/scripts/blog/blogCtrl.js
+++ app/scripts/blog/blogCtrl.js
@@ -3,4 +3,6 @@
 angular.module('jakenotjacobApp').controller('blogCtrl', function ($scope) {
 
   $scope.welcomeMessage = "Hello Jake";
+
+  $scope.body = "Lorem ipsum Jake lingwall is the best ever to do this on the whole planet";
 });
