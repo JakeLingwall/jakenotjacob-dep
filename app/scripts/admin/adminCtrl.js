c7d692208790ee5337c9dfe6f4463639bd2898a6
Jake Lingwall <jake.lingwall@domo.com>
2014-04-06 15:12:40 -0600

    mostly admin features. Lots of bugs, but almost MVP level

--- app/scripts/admin/adminCtrl.js
+++ app/scripts/admin/adminCtrl.js
@@ -3,23 +3,58 @@
 angular.module('jakenotjacobApp').controller('adminCtrl', function ($scope, $location, $routeParams) {
 
   var dataRef = new Firebase("https://jakenotjacob.firebaseio.com");
-  $scope.newPost = "hi";
+  $scope.post = {};
 
   //Will need to authenticate on this page. simple-login?
 
   $scope.leeroyJenkins = function(){
-    var newpost = {title: $scope.postTitle, body: $scope.newPost, date: new Date().getTime(), comments: []};
-    dataRef.child('posts').child(newpost.date).setWithPriority(newpost, newpost.date);
+    if(!$scope.post.date){
+      var newpost = {title: $scope.post.title, body: $scope.post.body, date: new Date().getTime(), comments: []};
+      dataRef.child('posts').child(newpost.date).setWithPriority(newpost, newpost.date);
+    } else{
+      if(!$scope.post.comments){
+        $scope.post.comments = [];
+      }
+      var newpost = {title: $scope.post.title, body: $scope.post.body, date: $scope.post.date, comments: $scope.post.comments};
+      dataRef.child('posts').child(newpost.date).setWithPriority(newpost, newpost.date);
+    }
   }
-  // dataRef.child($routeParams.project).on('value', function(data){
-  //   setScopeData(data.val());
-  // })
 
-  // function setScopeData (data){
-  //   $scope.project = data;
-  //   if(!$scope.$$phase){
-  //     $scope.$apply();
-  //   }
-  // }
+  $scope.editPost = function(){
+    $scope.post = this.post;
+  }
+
+  $scope.selectProject = function(){
+    $scope.selectedProject = this.project;
+  }
+
+  $scope.updateProjectCurrentGoal = function(){
+    dataRef.child(this.selectedProject.key).child("current_goal").set({text:this.selectedProject.current_goal.text, percent: this.selectedProject.current_goal.percent});
+  }
+  
+  $scope.postUpdate = function(){
+    dataRef.child(this.selectedProject.key).child("updates").push({date: new Date().getTime(), text: $scope.newUpdate});
+  };
+
+  $scope.updateMeta = function(){
+    dataRef.child(this.selectedProject.key).child("description").set({text: this.selectedProject.description.text, link: this.selectedProject.description.link});
+  }
+
+  $scope.newFile = function(){
+    dataRef.child(this.selectedProject.key).child("files").push($scope.newFile);    
+  }
+
+  //gets a lists of posts. Need a way to select on to edit. 
+  dataRef.on('value', function(data){
+    setScopeData(data.val());
+  })
+
+  function setScopeData (data){
+    $scope.data = data;
+    $scope.projects = _.omit(data, 'posts');
+    if(!$scope.$$phase){
+      $scope.$apply();
+    }
+  }
 
 });
--- app/scripts/project/projectCtrl.js
+++ app/scripts/project/projectCtrl.js
@@ -5,20 +5,13 @@ angular.module('jakenotjacobApp').controller('projectCtrl', function ($scope, $l
   var dataRef = new Firebase("https://jakenotjacob.firebaseio.com");
   dataRef.child($routeParams.project).on('value', function(data){
     setScopeData(data.val());
-  })
+  });
 
   function setScopeData (data){
     $scope.project = data;
     if(!$scope.$$phase){
       $scope.$apply();
     }
-  }
-
-  //Title
-  //Current goal title: status
-  //list of last updates
-  //project files
-  //list of posts that have the tags for this project
-  //project comments.
+  };
 
 });
