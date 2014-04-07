cb07e0b2658d472eed127e562e2d0277b778c37a
Jake Lingwall <jake.lingwall@domo.com>
2014-04-06 21:38:27 -0600

    good enough

--- app/scripts/main/mainCtrl.js
+++ app/scripts/main/mainCtrl.js
@@ -17,4 +17,21 @@ angular.module('jakenotjacobApp').controller('mainCtrl', function ($scope, $loca
   $scope.toFourthProject = function(){
     $location.path("/writings");
   }
+
+  $scope.notHovered = true;
+
+  //this is bad and lazy
+  $scope.toFacebook = function(){
+    window.location = 'https://www.facebook.com/jake.lingwall';
+  }
+  $scope.toTwitter = function(){
+    window.location = 'https://twitter.com/JakeLingwall';
+  }  
+  $scope.toGithub = function(){
+    window.location = 'https://github.com/JakeLingwall';
+  }  
+  $scope.toLinkedin = function(){
+    window.location = 'http://www.linkedin.com/pub/jake-lingwall/28/b52/b71/';
+  }
+
 });
